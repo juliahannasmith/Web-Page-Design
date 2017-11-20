53554280f945bd280de3f6acb5d137cea1bcb75e
Julia H Smith <js44684@srcs.loc>
2017-11-20 11:34:24 -0800

    date function

--- team_website_functions.js
+++ team_website_functions.js
@@ -17,4 +17,13 @@ randomGreeting()
 
 
 
-dateMessage()
+function dateMessage(){
+    var today = new Date();
+        var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+        var day = today.getDay();
+        var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+        var month = today.getMonth();
+        var date = today.getDate();
+        var year = today.getFullYear();
+       return date
+}
