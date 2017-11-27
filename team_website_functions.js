0d229ce60c05c461ef760c99922c7cf40c71d327
Katherine R Linnenkohl <kl88081@srcs.loc>
2017-11-27 11:19:00 -0800

    function

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,8 +1,8 @@
-randomGreeting()
+function randomGreeting(){
             var myArray = ['Hello!', 'Hi!', 'Bonjour!', 'Hola!' , "Welcome to Chili's" , 'Sah dude'];
             var rand = myArray[Math.floor(Math.random() * myArray.length)];
             document.write(rand);
-
+}
 
 
 
