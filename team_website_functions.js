eab4c83db6967628e357f9ac9585f75a911799de
Julia H Smith <js44684@srcs.loc>
2017-12-01 10:38:24 -0800

    day message

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,7 +1,7 @@
-function randomGreeting(){
-            var myArray = ['Hello!', 'Hi!', 'Bonjour!', 'Hola!' , "Welcome to Chili's" , 'Sah dude']
-    return myArray[Math.floor(Math.random() * myArray.length)
-}
+function randomGreeting() {
+    var myArray = ['Hello!', 'Hi!', 'Bonjour!', 'Hola!', "Welcome to Chili's", 'Sah dude']
+    return myArray[Math.floor(Math.random() * myArray.length)]
+    }
 
 
 
@@ -11,32 +11,20 @@ function randomGreeting(){
 
 
 
-
-
-
-
-
-
-
-
-function dateMessage(){
-    var today = new Date();
+    function dateMessage() {
+        var today = new Date();
         var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var day = today.getDay();
         var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         var month = today.getMonth();
         var date = today.getDate();
         var year = today.getFullYear();
-    return dayArray[day] + ", " + monthArray[month] + " " + [date] + ", " + [year]
-}
+        return dayArray[day] + ", " + monthArray[month] + " " + [date] + ", " + [year]
+    }
 
 
 
-function birthdayCountDown(){
-
-
-
-
+    function birthdayCountDown() {
 
 
 
@@ -46,4 +34,4 @@ function birthdayCountDown(){
 
 
 
-}
+    }
