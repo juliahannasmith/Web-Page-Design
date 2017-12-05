b259aa66a9151cfff18fcbaf1c218ed4e572fc0d
Nicole  Lescano <nl77656@srcs.loc>
2017-12-05 11:20:30 -0800

    the start of the new function

--- team_website_functions.js
+++ team_website_functions.js
@@ -12,72 +12,57 @@ function greeting() {
 
 
 
-    function dateMessage() {
-        var today = new Date();
-        var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        var day = today.getDay();
-        var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var month = today.getMonth();
-        var date = today.getDate();
-            if (date == 1 || date == 21 || date == 31){
-                dayEnding = "st";
-            }else if (date == 2 || date == 22){
-                 dayEnding = "nd";
-            }else if (date == 3 || date == 23){
-                dayEnding = "rd";
-            }else{
-                 dayEnding = "th";
-            }
-        var year = today.getFullYear();
-<<<<<<< HEAD
-       return today
+function dateMessage() {
+    var today = new Date();
+    var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var day = today.getDay();
+    var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = today.getMonth();
+    var date = today.getDate();
+    if (date == 1 || date == 21 || date == 31) {
+        dayEnding = "st";
+    } else if (date == 2 || date == 22) {
+        dayEnding = "nd";
+    } else if (date == 3 || date == 23) {
+        dayEnding = "rd";
+    } else {
+        dayEnding = "th";
+    }
+    var year = today.getFullYear(); <<
+    << << < HEAD
+    return today
 }
 
 
 
-function birthdayCountDown(){
+function birthdayCountDown(bdate) {
+
 
     var end = new Date();
-    var seconds = math.floor
-    var minutes = math.floor
-    var hours = math.floor
+    var birthday = new Date(birthdate);
+    var currenttime = today.getTime();
+    var birthtime = birthday.getTime();
+
+    if ((birthtime - currenttime) < 0) {
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+    }
 
 
 
 
 
 }
-
-
-
-
-
-
-=======
-     return dayArray[day] + ", " + monthArray[month] + " " + [date] + dayEnding + ", " + [year]
-    }
->>>>>>> origin/master
-
-
-
-    function birthdayCountDown() {
-
-
-
-
-
-
-
-
-
-<<<<<<< HEAD
-
-=======
-    }
->>>>>>> origin/master
