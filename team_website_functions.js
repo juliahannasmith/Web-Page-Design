4b7986e946fd55c7a0b720c0d08d36a25b1a128d
Nicole  Lescano <nl77656@srcs.loc>
2017-12-05 12:10:19 -0800

    updateded

--- team_website_functions.js
+++ team_website_functions.js
@@ -1,38 +1,54 @@
-function greeting(){
+function greeting() {
     var today = new Date();
     var day = today.getDay();
-    var hour = today.getHours();
-
-    if (hour >= 0 && hour <= 9){
-        if (day == 0 || day == 6){
-            return "Go back to sleep, its the weekend!";
-        }
-        else if (day == 1){
-            return "Ugh, its monday morning!";
+    var hour = today.getHour();
+    if (day == 6 || day == 0 ){
+        if (hour >= 0 && hour <= 10 ){
+            return "Go back to sleep, it's the weekend!"
         }
         else{
-            return "Good Morning!";
-        }
-    }
-    else if (hour >= 10 && hour <= 17){
-        if (day == 0 || day == 6){
-            return 	"Hope you are having a great weekend!";
-        }
-        else {
-            return "Good Day!";
-        }
-    }
-    else {
-        if(day == 0){
-            return 	"Enjoy your evening...tomorrow is Monday!";
-        }
-        else{
-            return "Good Evening!";
-        }
-    }
-}
 
+        }
+
+    }
+    else if (day == 1 && hour >= 0 && hour <= 8){
+        return "Ugh, it's Monday morning!"
+    }
+    else if (day == 2|| day == 3 || day == 4 || day == 5){
+        if (hour >= 0 && hour <= 9){
+            return "Good Morning"
+        }
+        else if (hour >= 10 && hour <= 17){
+            return "Good Day!"
+        }
+        else (hour >= 18 && hour <= 23){
+            return "Good Evening!"
+        }
+    }
+
+<<<<<<< HEAD
 function dateMessage() {
+    var today = new Date();
+    var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var day = today.getDay();
+    var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = today.getMonth();
+    var date = today.getDate();
+    if (date == 1 || date == 21 || date == 31) {
+        dayEnding = "st";
+    } else if (date == 2 || date == 22) {
+        dayEnding = "nd";
+    } else if (date == 3 || date == 23) {
+        dayEnding = "rd";
+    } else {
+        dayEnding = "th";
+    }
+    var year = today.getFullYear(); <<
+    << << < HEAD
+    return today
+}
+=======
+    function dateMessage() {
         var today = new Date();
         var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var day = today.getDay();
@@ -52,36 +68,28 @@ function dateMessage() {
         return dayArray[day] + ", " + monthArray[month] + " " + [date] + dayEnding + ", " + [year]
     }
 
+
+
+
 function birthdayCountDown(bdate) {
-
-
-    var end = new Date();
-    var seconds = math.floor
-    var minutes = math.floor
-    var hours = math.floor
-    var birthday = new Date(birthdate);
-    var currenttime = today.getTime();
-    var birthtime = birthday.getTime();
-
-    if ((birthtime - currenttime) < 0) {
-
-
-birthday.setFullYear(today.getFullYear());
-
-
-
-
-
-
-
-
->>>>>>> origin/master
-
+    var today = new Date();
+    var mybirthday = new Date(birthdate);
+mybirthday.setFullYear (today.getFullYear());
+    if (today.getTime()> myBirthday.getTime()) {
+        mybirthday.setFullYear(today.getFullYear())
     }
 
 
 
 
+        }
 
-}
-l
+
+
+
+
+
+
+
+
+
