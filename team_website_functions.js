3891f2f0991577a0f78dd1b9352e7542e6776f2b
Nicole  Lescano <nl77656@srcs.loc>
2017-12-07 11:50:34 -0800

    finishing touches

--- team_website_functions.js
+++ team_website_functions.js
@@ -25,44 +25,40 @@ function greeting() {
         }
     }
 }
+
 function dateMessage() {
-        var today = new Date();
-        var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        var day = today.getDay();
-        var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-        var month = today.getMonth();
-        var date = today.getDate();
-            if (date == 1 || date == 21 || date == 31){
+    var today = new Date();
+    var dayArray = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var day = today.getDay();
+    var monthArray = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var month = today.getMonth();
+    var date = today.getDate();
+    if (date == 1 || date == 21 || date == 31) {
         dayEnding = "st";
-            }else if (date == 2 || date == 22){
+    } else if (date == 2 || date == 22) {
         dayEnding = "nd";
-            }else if (date == 3 || date == 23){
+    } else if (date == 3 || date == 23) {
         dayEnding = "rd";
-            }else{
+    } else {
         dayEnding = "th";
-            }
-        var year = today.getFullYear();
-        return dayArray[day] + ", " + monthArray[month] + " " + [date] + dayEnding + ", " + [year]
+    }
+    var year = today.getFullYear();
+    return dayArray[day] + ", " + monthArray[month] + " " + [date] + dayEnding + ", " + [year]
 }
 
-function birthdayCountDown(bday) {
-var today = new Date();
-    var bday = new Date(birthdate)
-   var currenttime = today.getTime();
+function birthdayCountDown(birthday) {
+    var today = new Date();
+    var birthday = new Date(birthday)
+    var currenttime = today.getTime();
     var btime = birthday.getTime();
 
-    if (( btime - currenttime) < 0) {
-    birthday.setFullYear(today.getFullYear)
+    if ((btime - currenttime) < 0) {
+        birthday.setFullYear(today.getFullYear)
+        if ((birthday.getTime() - today.getTime() < 0)) {
+            birthday.setFullYear(birthday.getFullYear() + 1)
+        }
 
-
-     var daysuntil = Math.floor((birthday.getTime() / 86400000) - (today.getTime() / 86400000));
-
-
-
+        var daysuntil = Math.floor((birthday.getTime() / 86400000) - (today.getTime() / 86400000));
     }
-    var diff = myBirthday.getTime() - today.getTime();
-
 
     document.write(daysuntil);
-
-
